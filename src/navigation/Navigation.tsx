3b6dd108f397375c26551a14fc1426be33d58650
Raul Garcia <rraulg5@gmail.com>
2021-08-01 10:25:52 -0500

    Navigation initial set up

---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { HomeScreen } from '../screens/HomeScreen';
+import { DetailsScreen } from '../screens/DetailsScreen';
+
+const Stack = createStackNavigator();
+
+export const Navigation = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: { backgroundColor: 'white' },
+      }}
+    >
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+    </Stack.Navigator>
+  );
+};
